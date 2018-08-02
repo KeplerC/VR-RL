9fcd947ff2d3988e838998e5efcca29df1f7663d
Your Name <you@example.com>
2018-08-02 16:00:23 -0700

    order

--- dqn/model.py
+++ dqn/model.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+
+from __future__ import division, print_function, unicode_literals
+
+# Handle arguments (before slow imports so --help can be fast)
+import argparse
+parser = argparse.ArgumentParser(
+    description="Train a DQN net for Atari games.")
+
+# Important hparams
+parser.add_argument("-g", "--game", type=str, default="Pong")
+parser.add_argument("-n", "--number-steps", type=int, default=1000000, help="total number of training steps")
+parser.add_argument("-e", "--explore-steps", type=int, default=100000, help="total number of explorartion steps")
+parser.add_argument("-c", "--copy-steps", type=int, default=4096, help="number of training steps between copies of online DQN to target DQN")
+parser.add_argument("-l", "--learn-freq", type=int, default=4, help="number of game steps between each training step")
+
+# Irrelevant hparams
+parser.add_argument("-s", "--save-steps", type=int, default=10000, help="number of training steps between saving checkpoints")
+parser.add_argument("-r", "--render", action="store_true", default=False, help="render the game during training or testing")
+parser.add_argument("-t", "--test", action="store_true", default=False, help="test (no learning and minimal epsilon)")
+parser.add_argument("-v", "--verbosity", action="count", default=1, help="increase output verbosity")
+parser.add_argument("-j", "--jobid", default="123123", help="SLURM job ID")
+
+args = parser.parse_args()
+
 from collections import deque
 import gym
 import numpy as np
@@ -10,33 +34,25 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
-from config import *
 
-
-'''
-use vr gum environment 
-'''
 import gym_foo
+#env = wrap_dqn(gym.make("{}NoFrameskip-v4".format(args.game)))
 env = gym.make("foo-v0")
 
-
 def q_network(net, name, reuse=False):
-    '''
-    Deep Q Learning Network
-    current architecture:
-    =input
-    LSTM layer
-    fully connected relu
-    fully connected layer
-    =output action space
-    
-    '''
     with tf.variable_scope(name, reuse=reuse) as scope:
         initializer = tf.contrib.layers.variance_scaling_initializer()
-        lstm_cell = tf.contrib.rnn.BasicLSTMCell(num_time_step,state_is_tuple=True)
+        '''
+        for n_maps, kernel_size, strides, padding, activation in zip(
+                [32, 64, 64], [(8,8), (4,4), (3,3)], [4, 2, 1],
+                ["SAME"] * 3 , [tf.nn.relu] * 3):
+            net = tf.layers.conv2d(net, filters=n_maps, kernel_size=kernel_size, strides=strides, 
+                padding=padding, activation=activation, kernel_initializer=initializer)
+        '''
+        lstm_cell = tf.contrib.rnn.BasicLSTMCell(128,state_is_tuple=True)
         lstm_state_in = lstm_cell.zero_state(1, tf.float32)
         net,lstm_state_out = tf.nn.dynamic_rnn(inputs = net, cell = lstm_cell, dtype=tf.float32, initial_state = lstm_state_in, scope = "lstm_cell")
-
+        #net = tf.layers.dense(tf.contrib.layers.flatten(net), 256, activation=tf.nn.relu, kernel_initializer=initializer)
         net = tf.layers.dense(net, 256, activation=tf.nn.relu, kernel_initializer=initializer)
         net = tf.contrib.layers.flatten(net)
         net = tf.layers.dense(net, env.action_space.n, kernel_initializer=initializer)
@@ -44,14 +60,15 @@ def q_network(net, name, reuse=False):
     trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope.name)
     return net, trainable_vars
 
-
-'''
-training step
-'''
+# Now for the training operations
+learning_rate = 1e-4
+training_start = 10000  # start training after 10,000 game steps
+discount_rate = 0.99
+batch_size = 1
 
 with tf.variable_scope("train"):
-    X_state = tf.placeholder(tf.float32, shape=[None, num_analyzer * num_feature, num_time_step])
-    X_next_state = tf.placeholder(tf.float32, shape=[None, num_analyzer * num_feature, num_time_step])
+    X_state = tf.placeholder(tf.float32, shape=[None, 56, 128])
+    X_next_state = tf.placeholder(tf.float32, shape=[None, 56, 128])
     X_action = tf.placeholder(tf.int32, shape=[None])
     X_done = tf.placeholder(tf.float32, shape=[None])
     X_rewards = tf.placeholder(tf.float32, shape=[None])
@@ -69,14 +86,17 @@ with tf.variable_scope("train"):
     optimizer = tf.train.AdamOptimizer(learning_rate)
     training_op = optimizer.minimize(loss, global_step=global_step)
 
-# copy the online DQN to the target DQN
+# We need an operation to copy the online DQN to the target DQN
 copy_ops = [target_var.assign(online_var)
             for target_var, online_var in zip(target_vars, online_vars)]
 copy_online_to_target = tf.group(*copy_ops)
 
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
 # Let's implement a simple replay memory
 replay_memory = deque([], maxlen=10000)
-import numpy as np
+
 def sample_memories(batch_size):
     indices = np.random.permutation(len(replay_memory))[:batch_size]
     cols = [[], [], [], [], []] # state, action, reward, next_state, continue
@@ -87,9 +107,12 @@ def sample_memories(batch_size):
     cols = [np.array(col) for col in cols]
     return cols
 
+# And on to the epsilon-greedy policy with decaying epsilon
+eps_min = 0.01
+eps_max = 1.0 if not args.test else eps_min
 
 def epsilon_greedy(q_values, step, bound = None):
-    epsilon = max(eps_min, eps_max - (eps_max-eps_min) * step / explore_steps)
+    epsilon = max(eps_min, eps_max - (eps_max-eps_min) * step / args.explore_steps)
     rand_action_within_bound = np.random.randint(env.action_space.n)
     while(rand_action_within_bound in bound):
         rand_action_within_bound = np.random.randint(env.action_space.n)
@@ -104,8 +127,9 @@ def epsilon_greedy(q_values, step, bound = None):
         else:
             return potential_q_values
 
-# initialize training variables 
-done = True 
+done = True # env needs to be reset
+
+# We will keep track of the max Q-Value over time and compute the mean per game
 loss_val = np.infty
 game_length = 0
 total_max_q = 0
@@ -113,31 +137,30 @@ mean_max_q = 0.0
 returnn = 0.0
 returns = []
 steps = []
-init = tf.global_variables_initializer()
-saver = tf.train.Saver()
+path = os.path.join(args.jobid, "model")
 state, reward, done, info = env.step(env.action_space.sample())
-
 with tf.Session() as sess:
     if os.path.isfile(path + ".index"):
         saver.restore(sess, path)
     else:
         init.run()
         copy_online_to_target.run()
-    for step in range(num_steps):
+    for step in range(args.number_steps):
         training_iter = global_step.eval() 
         if step % 50 == 3: # game over, start again
             print(reward, info)
             done = True
-            if verbosity > 0:
+            if args.verbosity > 0:
                 print("Step {}/{} ({:.1f})% Training iters {}   "
                       "Loss {:5f}    Mean Max-Q {:5f}   Return: {:5f}".format(
-                step, num_steps, step * 100 / num_steps,
+                step, args.number_steps, step * 100 / args.number_steps,
                 training_iter, loss_val, mean_max_q, returnn))
                 sys.stdout.flush()
         if done:
             state = env.reset()
-
-        state = state.reshape((num_analyzer * num_feature, num_time_step))
+        #if args.render:
+        #    env.render()
+        state = state.reshape((56, 128))
         # Online DQN evaluates what to do
         q_values = online_q_values.eval(feed_dict={X_state: [state]})
         action = epsilon_greedy(q_values, step, info["bound"])
@@ -146,11 +169,11 @@ with tf.Session() as sess:
         next_state, reward, done, info = env.step(action)
         returnn += reward
 
-        # memorization 
+        # Let's memorize what happened
         replay_memory.append((state, action, reward, next_state, done))
         state = next_state
 
-        if istest:
+        if args.test:
             continue
 
         # Compute statistics for tracking progress (not shown in the book)
@@ -164,7 +187,7 @@ with tf.Session() as sess:
             total_max_q = 0.0
             game_length = 0
 
-        if step < training_start or step % learn_freq != 0:
+        if step < training_start or step % args.learn_freq != 0:
             continue # only train after warmup period and at regular intervals
         
         # Sample memories and train the online DQN
@@ -178,12 +201,11 @@ with tf.Session() as sess:
         X_next_state: X_next_state_val})
 
         # Regularly copy the online DQN to the target DQN
-        if step % copy_steps == 0:
+        if step % args.copy_steps == 0:
             copy_online_to_target.run()
 
         # And save regularly
-        if step % save_steps == 0:
+        if step % args.save_steps == 0:
             saver.save(sess, path)
-            np.save(os.path.join(jobid, "{}.npy".format(jobid)), np.array((steps, returns)))
-
+            np.save(os.path.join(args.jobid, "{}.npy".format(args.jobid)), np.array((steps, returns)))
 
